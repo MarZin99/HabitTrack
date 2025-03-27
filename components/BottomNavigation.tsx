67be566d95fab4d2a42096db048de1ad19a1389e
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2025-03-27 15:56:54 +0100

    Fix white bar on bottom navigation

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -14,10 +14,11 @@ const Tab = createBottomTabNavigator();
 
    return (
     <Tab.Navigator screenOptions={{
-      tabBarStyle: styles.tabbarWrapper,
-      tabBarBackground: () => {
-        return <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} /> 
-      }
+      tabBarStyle: styles.tabBarStyle,
+      tabBarItemStyle: styles.tabBarItemStyle,
+      tabBarBackground: () => (
+        <BlurView tint="light" intensity={70} style={{...StyleSheet.absoluteFillObject, backgroundColor: "transparent", height: 70}} /> 
+      )
     }}>
       <Tab.Screen name="Home" component={HomeScreen} options={{headerShown : false, tabBarIcon: ({color}) => <FontAwesome size={28} name="home" color={color} />,}}/>
       <Tab.Screen name="Settings" component={SettingsScreen} options={{headerShown : false, tabBarIcon: ({color}) => <FontAwesome size={28} name="gear" color={color} />,}} />
@@ -29,24 +30,18 @@ export default BottomNavigation
 
 
 const styles = StyleSheet.create({
-    tabbarWrapper: {
+  tabBarStyle: {
       overflow: "hidden",
       position: "absolute",
       margin: 10,
       alignItems: "center",
-      elevation: 5,
       height: 70,
-      backgroundColor: "rgba(0,0,0,0.0)",
-      paddingTop: 5,
       borderRadius: 20,
-      shadowColor: "#7F5DF0",
-      shadowOffset: {
-        width: 0,
-        height: 10,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.5,   
+      elevation: 0,
     },
-
+    tabBarItemStyle: {
+      flexDirection: "row",
+      alignItems: "center"
+    }
   });
 
